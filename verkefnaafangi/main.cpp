3008ee235377a22b79cdc7702cbe541807402e3a
skulia15 <skuliarnarsson@gmail.com>
2015-11-27 14:39:25 +0000

    nothing

--- verkefnaafangi/main.cpp
+++ verkefnaafangi/main.cpp
@@ -31,7 +31,7 @@ int main()
 
     subject.push_back(scientists()); //default constructor
 
-    //callDefaultMenu();
+    callDefaultMenu();
 
     inputInfo(subject);
 
@@ -44,39 +44,39 @@ int main()
 }
 
 void callDefaultMenu(){
-        char a;
-        bool loop = true;
+    char a;
+    bool loop = true;
 
-        while(loop == true)
+    while(loop == true)
+    {
+        cout << "Press 1 to add a person" << endl;
+        cout << "Press 2 to print all persons" << endl;
+        cout << "Press 3 to search" << endl;
+        cout << "Press 4 to quit" << endl;
+
+        cin >> a;
+
+        switch(a)
         {
-            cout << "Press 1 to add a person" << endl;
-            cout << "Press 2 to print all persons" << endl;
-            cout << "Press 3 to search" << endl;
-            cout << "Press 4 to quit" << endl;
-
-            cin >> a;
-
-            switch(a)
-            {
-            case '1':
-                 //input();
-                 break;
-            case '2':
-                //print();
-                break;
-            case '3':
-               //search();
-               break;
-             case '4':
-                cout << "Quitting." << endl;
-                loop = false;
-                break;
-             default:
-                cout << "Error in command, try again." << endl;
-                callDefaultMenu();
-                break;
-            }
+        case '1':
+             //input();
+             break;
+        case '2':
+            //print();
+            break;
+        case '3':
+           //search();
+           break;
+         case '4':
+            cout << "Quitting." << endl;
+            loop = false;
+            break;
+         default:
+            cout << "Error in command, try again." << endl;
+            callDefaultMenu();
+            break;
         }
+    }
 }
 
 void inputInfo(vector<scientists> subject){
