ee1c5686ebcacf96eabb524d03014c80e7be3362
skulia15 <skuliarnarsson@gmail.com>
2015-11-29 17:54:08 +0000

    Hellingur af flottum features og bug fixes

--- verkefnaafangi/main.cpp
+++ verkefnaafangi/main.cpp
@@ -4,43 +4,38 @@
 #include <cstdlib>
 #include <vector>
 #include <fstream>
-#include <ostream>
 
 using namespace std;
 
 struct scientists{
     string firstName;
     string lastName;
-    char personSex;
+    string personSex;
     int yearOfBirth;
     int yearOfDeath;
 };
 
 void callDefaultMenu(vector<scientists>& subject);
 void inputInfo(vector<scientists>& subject);
-void output(vector<scientists>& subject);
+void output(vector<scientists>& subject, int persons);
+void print();
 
 int main()
-{ 
+{
     //ToDo list
-    //Check if vector pushes back values correctly
-    //Figure out how to print contents of vector
+    //Check if vector pushes back values correctly.
+    //Figure out how to print contents of vector.
     //Create a function to save the info into a file.
-    //Create function to read from file. - without everwriting
     //Print options f.ex alphabetical order
-    //Implement a search function
-    //Error handling
-    //Cool extra features
+    //Create a function to remove a computer scientist.
+    //Implement a search function.
+    //Error handling.
+    //Cool extra features.
 
-    vector<scientists> subject;
-
-    subject.push_back(scientists()); //default constructor
-    output(subject);
+    vector<scientists> subject;  //vector of persons
 
     callDefaultMenu(subject);
 
-    for(unsigned int i=0; i < subject.size(); ++i)
-      //cout << subject[i] << ' ';    þarf að yfirskifa << operator til þess að fá til að skrifa út?
     return 0;
 }
 
@@ -48,16 +43,13 @@ void callDefaultMenu(vector<scientists>& subject){
     char a;
     bool loop = true;
 
-    while(loop == true)
-    {
-        cout << "-----------Menu-----------"<<endl;
-        cout << endl;
-        cout << " Please select a option  "<<endl;
-        cout << " 1. to add a person" << endl;
-        cout << " 2. to print all persons" << endl;
-        cout << " 3. to search" << endl;
-        cout << " 4. to quit" << endl;
-        cout << endl;
+    while(loop == true){
+        cout << "-----------Menu-----------";
+        cout << " \n Please select a option:  "<<endl;
+        cout << " \n 1. To add a computer scientist" << endl;
+        cout << " 2. To print all persons" << endl;
+        cout << " 3. To search" << endl;
+        cout << " 4. To quit" << endl;
         cout << "--------------------------"<<endl;
         cin >> a;
 
@@ -66,7 +58,7 @@ void callDefaultMenu(vector<scientists>& subject){
              inputInfo(subject);
              break;
         case '2':
-            //print();
+            print();
             break;
         case '3':
            //search();
@@ -86,54 +78,90 @@ void inputInfo(vector<scientists>& subject){
     scientists scientist1;
     char answer = 0;
     int persons;
+    bool error = false;
+    string sex;
+    char sexTemp;
 
     cout << "How many persons would you like to add? ";
     cin >> persons;
 
-    for (int i = 0;i < persons; i++){
+    for (int i = 1;i <= persons; i++){
         if (i == 0){
-            cout << "Enter the info for the first person: " << endl;
-        }
+            cout << "\nEnter the info for the first person: " << endl;}
         else {
-            cout << "Enter the info for the next person: " << endl;
-        }
+            cout << "Enter the info for the next person: " << endl;}
 
         cout << "Person's first name: ";
         cin >> scientist1.firstName;
-        cout << " Last name: ";
+        cout << "Last name: ";
         cin >> scientist1.lastName;
         cout << endl;
 
-        cout << "Sex: (m/f) " ;
-        cin >> scientist1.personSex;
+        do{
+            cout << "Sex: (m/f) " ;
+            cin >> sexTemp;
+            if (sexTemp == 'm'){ sex = "Male"; error = true;}
+            else if (sexTemp == 'f'){ sex = "Female"; error = true;}
+            else cout << "Input was invalid try again." << endl;
+        } while (error == false);
+        scientist1.personSex = sex;
         cout << endl;
 
         cout << "Year of Birth: ";
         cin >> scientist1.yearOfBirth;
         cout << endl;
 
-        cout << "Is the person still alive? (y/n) ";
-        cin >> answer;
-
-        if (answer == 'n'||answer == 'N'){
-            cout << "\nYear of Death: ";
-            cin >> scientist1.yearOfDeath;
-        }
+        error = false;
+        do{
+            cout << "Is the person still alive? (y/n) ";
+            cin >> answer;
+            if (answer == 'n'||answer == 'N'){
+                cout << "\nYear of Death: ";
+                cin >> scientist1.yearOfDeath;
+                error = true;}
+            else if (answer == 'y'||answer == 'Y'){
+                scientist1.yearOfDeath = 0;
+                error = true;}
+            else cout << "Input was invalid try again." << endl;
+        } while (error == false);
         cout << endl;
 
-        subject.push_back(scientist1);
-        output(subject);
+        subject.push_back(scientist1);  //The struct is pushed on to the vector
     }
+    output(subject, persons);
 }
 
-void output(vector<scientists>& subject){ //drag að ofstream/ifstream falli
-    ifstream document;
-    ofstream writeToFile;
-    document.open("list.txt");
-    writeToFile.open("list.txt");
-    writeToFile.close();
-    document.close();
-
-
+void output(vector<scientists>& subject, int persons){
+    ofstream myfile;
+    myfile.open("save.txt", ios::out | ios::app); //For adding without overwriting
+    if (myfile.is_open()){
+        for (int i = 0; i < persons; i++){  //Needs to be modified, not adding persons correctly. 1 off error.
+            myfile << "Name: " << subject[i].firstName;
+            myfile << " " << subject[i].lastName << endl;
+            myfile << "Sex: "<< subject[i].personSex << endl;
+            myfile << "Year of birth: " << subject[i].yearOfBirth << endl;
+            if(subject[i].yearOfDeath == 0){  //if still alive then marks as "Still Alive"
+                myfile << subject[i].firstName << " " << subject[i].lastName << " is still alive." << endl;}
+            else {
+                myfile << "Year of death: " << subject[i].yearOfDeath << endl;}
+            myfile << endl;
+        }
+        myfile.close();
+    }
+    else cout << "Unable to open file";
 }
 
+void print(){
+    ifstream myfile;
+    string line;
+    myfile.open("save.txt");
+    if (myfile.is_open()){
+    cout << "------Displaying Persons------" << endl;
+    while (getline (myfile,line)){
+      cout << line << '\n';
+    }
+    myfile.close();
+  }
+
+  else cout << "Unable to open file";
+}
