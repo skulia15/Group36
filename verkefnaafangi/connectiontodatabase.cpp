769ee1cdeb55588e6407eeaa81e1a177bcc5f4e6
skulia15 <skuliarnarsson@gmail.com>
2015-12-07 14:57:45 +0000

    conflicts resolved

--- verkefnaafangi/connectiontodatabase.cpp
+++ verkefnaafangi/connectiontodatabase.cpp
@@ -12,32 +12,18 @@ connectionToDataBase::connectionToDataBase()
 
     db.open();
 
-     QSqlQuery query(db);
+    QSqlQuery query(db);
 
     if(db.open())
-<<<<<<< HEAD
          qDebug()<<"========== Opened the Database ==========" << endl;
     else
          qDebug()<<"========== Error: "<<db.lastError().text() << " ==========" << endl;
 
- string queryCreate ="CREATE TABLE students(id INTEGER, name VARCHAR, email VARCHAR, age INTEGER); ";
+ string queryCreate = "CREATE TABLE Scientists(id INTEGER PRIMARY KEY AUTOINCREMENT, firstName VARCHAR NOT NULL, lastName VARCHAR NOT NULL, sex VARCHAR, YoB INTEGER, YoD INTEGER); ";
+
  if(query.exec(QString(queryCreate.c_str()))){
     cout << "========== The Database was made successfully! =========="<< endl;}
  else {cout<< "========== The Database already exists! ==========" << endl;}
-=======
-         qDebug() << "Database was opened" << endl;
-    else
-         qDebug()<<"Database was closed" << db.lastError().text();
 
-    string queryCreate = "CREATE TABLE Scientists(id INTEGER PRIMARY KEY AUTOINCREMENT, firstName VARCHAR NOT NULL, lastName VARCHAR NOT NULL, sex VARCHAR, YoB INTEGER, YoD INTEGER); ";
-    if(query.exec(QString(queryCreate.c_str()))){
-        cout << "The Database was made successfully "<< endl;}
-    else {
-<<<<<<< HEAD
-        cout<< "A new Database was NOT made " << endl;}
-=======
-        cout<< "The Database was NOT made " << endl;}
->>>>>>> 164d4166f55dccc8e0d01206b4ee6984ba400242
->>>>>>> e6e9f8a5556062e155d4ea892f5b42c537d9591a
 }
 
--- verkefnaafangi/manualInsertScientist.cpp
+++ verkefnaafangi/manualInsertScientist.cpp
@@ -9,18 +9,8 @@ Scientists::Scientists(){
    firstName = "";
    lastName = "";
    sex = "";
-<<<<<<< HEAD
-   YoB = 0;
-   YoD = 0;
-=======
-<<<<<<< HEAD
    YoB = "";
    YoD = "";
-=======
-   YoB;
-   YoD;
->>>>>>> 164d4166f55dccc8e0d01206b4ee6984ba400242
->>>>>>> e6e9f8a5556062e155d4ea892f5b42c537d9591a
 };
 
 void Scientists::manualInput(){
@@ -28,29 +18,9 @@ void Scientists::manualInput(){
     QSqlDatabase db;
     QSqlQuery query(db);
 
-<<<<<<< HEAD
     string queryPrint = "SELECT * FROM Scientists";
     query.exec(QString(queryPrint.c_str()));
 
-=======
-<<<<<<< HEAD
-        QSqlDatabase db;
-        QSqlQuery query(db);
-        //string id,name,email, age;
-
-        cout << "Please enter the computer scientists information." << endl;
-        cout << "First name: ";
-        cin >> firstName;
-        cout << "Last name: ";
-        cin >> lastName;
-        cout << "Sex: ";
-        cin >> sex;
-        cout << "Year of Birth: ";
-        cin >> YoB;
-        cout << "Year of Death: ";
-        cin >> YoD;
-=======
->>>>>>> e6e9f8a5556062e155d4ea892f5b42c537d9591a
     cout << "Please enter the computer scientists information." << endl;
     cout << "First name: ";
     cin >> firstName;
@@ -62,8 +32,6 @@ void Scientists::manualInput(){
     cin >> YoB;
     cout << "Year of Death: ";
     cin >> YoD;
->>>>>>> 164d4166f55dccc8e0d01206b4ee6984ba400242
-
 
 
     query.prepare("SELECT * FROM Scientists WHERE id = :id;)");
@@ -75,28 +43,18 @@ void Scientists::manualInput(){
     query.prepare("SELECT * FROM Scientists WHERE sex = :sex;)");
     query.bindValue(":sex", QString::fromStdString(sex));
     query.prepare("SELECT * FROM Scientists WHERE YoB = :YoB;)");
-    query.bindValue(":YoB", QString(YoB));
+    query.bindValue(":YoB", QString::fromStdString(YoB));
     query.prepare("SELECT * FROM Scientists WHERE YoD = :YoD;)");
-    query.bindValue(":YoD", QString(YoD));
+    query.bindValue(":YoD", QString::fromStdString(YoD));
 
 
-   string player = "INSERT INTO Scientists (firstName, lastName, sex, YoB) values ('"+firstName+"', '"+lastName+"', '"+sex+"', '"+YoB+"')";
-   //Vantar að adda YoB og YoD, fæ alltaf errors....
-
-<<<<<<< HEAD
-      string player ="INSERT INTO students (firstName, lastName, sex, YoB, YoD) values ('"+firstName+"', '"+lastName+"', '"+sex+"', '"+YoB+"', '"+YoD+"')";
+      string player ="INSERT INTO Scientists (firstName, lastName, sex, YoB, YoD) values ('"+firstName+"', '"+lastName+"', '"+sex+"', '"+YoB+"', '"+YoD+"')";
 
         if(query.exec(QString(player.c_str()))){
 
             cout<<"========== Setti inn i Database =========="<<endl;
         }else cout << "========== Setti Ekki inn i Database =========="<<endl;
-=======
-    if(query.exec(QString(player.c_str()))){
-        cout << "Successfully added to Database" << endl;
->>>>>>> 164d4166f55dccc8e0d01206b4ee6984ba400242
-    }
-    else
-        cout << "Was NOT added to the Database" << endl;
+
 }
 
 
