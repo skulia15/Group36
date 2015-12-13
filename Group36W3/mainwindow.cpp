0544d2f5c9d0cdcb9a382cd2bad4b31b8b7e8270
Sverrir V <sverrirvh@gmail.com>
2015-12-13 00:29:11 +0000

    DisplayÞSciÞkomid

--- Group36W3/mainwindow.cpp
+++ Group36W3/mainwindow.cpp
@@ -35,21 +35,40 @@ void MainWindow::displayScientists(std::vector<Scientist> scientists)//Sverrir, 
         ui->table_showAllScientists->setRowCount(scientists.size());
         ui->table_showAllScientists->setColumnCount(5);
 
+             for(unsigned int row=0;row<scientists.size();row++)
+             {
+                Scientist currentScientists = scientists.at(row);
+                QTableWidgetItem* newItem = new QTableWidgetItem();
+                newItem->setText(QString::number(currentScientists.getId()));
+                ui->table_showAllScientists->setItem(row,0,newItem);
 
-             for(unsigned int row=0;row<scientists.size();row++){
-
-                 Scientist currentScientists = scientists.at(row);
-                 QTableWidgetItem* newItem = new QTableWidgetItem();
-                newItem->setText(QString::fromStdString(currentScientists.getName()));
-
-                 ui->table_showAllScientists->setItem(row,1,newItem);
+                QTableWidgetItem* newItem1 = new QTableWidgetItem();
+                newItem1->setText(QString::fromStdString(currentScientists.getName()));
+                ui->table_showAllScientists->setItem(row,1,newItem1);
 
 
-             }
+                QTableWidgetItem* newItem2 = new QTableWidgetItem();
+                if(currentScientists.getSex()==0){
+                     newItem2->setText("Male");                }
+                else{newItem2->setText("Female");}
+
+                ui->table_showAllScientists->setItem(row,2,newItem2);
+
+                QTableWidgetItem* newItem3 = new QTableWidgetItem();
+                newItem3->setText(QString::number(currentScientists.getYearBorn()));
+                ui->table_showAllScientists->setItem(row,3,newItem3);
+
+                QTableWidgetItem* newItem4 = new QTableWidgetItem();
+                newItem4->setText(QString::number(currentScientists.getYearDied()));
+                ui->table_showAllScientists->setItem(row,4,newItem4);
+
+
+                //courtsey of https://forum.qt.io/topic/27584/fill-a-qtablewidget/10
+              }
 }
 
 
-void MainWindow::on_button_display_scientists_clicked()
+void MainWindow::on_button_display_scientists_clicked()//Sverrir, Button that calls on display
 {
     displayAllScientists();//Sverrir, Calls display.
     qDebug() << "Date:" << QDate::currentDate(); //Sverrir, Confirmation of button pusshed works
